b55efb6796575ce7a3311258fa70da05251d6062
Sebastian Matysiak <smatysiak@empik.com>
2021-04-26 08:53:51 +0200

    Add dimens and font styles

---
+++ src/resources/dimens.ts
@@ -0,0 +1,25 @@
+export const dimens = {
+  space: {
+    xxs: 2,
+    xs: 4,
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 32,
+    xxl: 48,
+    k: 64,
+    xxk: 96
+  },
+  fontSize: {
+    xxs: 10,
+    xs: 12,
+    s: 14,
+    m: 16,
+    l: 18,
+    x: 20,
+    xl: 22,
+    xxl: 28,
+    k: 34,
+    xk: 40
+  }
+}
---
+++ src/resources/fonts.ts
@@ -0,0 +1,7 @@
+import { dimens } from './dimens'
+
+export const fonts = {
+  regular14: {
+    fontSize: dimens.fontSize.m
+  }
+}
